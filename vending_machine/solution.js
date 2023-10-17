ba0e49608f478271ae7cc1051255dbf5557cc8b8
Your Name <your_email@address>
2023-10-17 15:28:18 +0100

    vending machine - change and quantity handled

--- vending_machine/solution.js
+++ vending_machine/solution.js
@@ -1,5 +1,5 @@
 const items = [
-  { name: "Smarties", code: "A01", quantity: 0, price: 0.6 },
+  { name: "Smarties", code: "A01", quantity: 1, price: 0.6 },
   { name: "Caramac Bar", code: "A02", quantity: 5, price: 0.6 },
   { name: "Dairy Milk", code: "A03", quantity: 1, price: 0.65 },
   { name: "Freddo", code: "A04", quantity: 1, price: 0.25 },
@@ -13,10 +13,21 @@ class VendingMachine {
 
   vend(selection, itemMoney) {
     console.log(selection, itemMoney);
-    let selectedObj = this.items.find((e) => e.code === selection);
+    let selectedObj = this.items.find(item => item.code === selection);
     console.log(selectedObj);
-    if (itemMoney < selectedObj.price) return "Not enough money!";
-    if (selectedObj.quantity === 0) return `${selectedObj.name}: Out of stock!`;
+
+    const { name, quantity, price } = selectedObj
+
+    if (itemMoney < price) return "Not enough money!";
+    if (quantity === 0) return `${name}: Out of stock!`;
+
+    selectedObj.quantity -= 1
+    this.money += price
+
+    let change = (itemMoney - price).toFixed(2)
+    const message = `Vending ${name}`
+
+    return change > 0 ? `${message} with ${change} change.` : message
   }
 }
 
